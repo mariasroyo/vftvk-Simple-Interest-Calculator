13db02d6ed8c832bbe6c1b1fecba63f33721b6f6
mariasroyo <77736807+mariasroyo@users.noreply.github.com>
2021-01-21 13:42:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    principal = Number(document.getElementById("principal").value);
+    rate = Number(document.getElementById("rate").value);
+    years = document.getElementById("years").value;
+    interest = (document.getElementById("principal").value)*years*rate*principal/100;
     
+    year = Number(new Date().getFullYear().value);
+    finalyear = year + years;
+
+    document.getElementById("result").innerHTML = finalyear;
+   
+}
+
+function rangeVal() {
+    range = document.getElementById("rate").value;
+    document.getElementById("rateSel").innerHTML = range;
 }
         
